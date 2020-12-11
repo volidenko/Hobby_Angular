700227e757c4d4ee5caeee95f63d1b3186e4ed63
volidenko <volidenko@.i.ua>
2020-12-11 03:55:26 +0200

    comm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HobbyComponent } from './hobby/hobby.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HobbyComponent
   ],
   imports: [
     BrowserModule
